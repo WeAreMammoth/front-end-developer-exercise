51a98b9df1305e95e4a0aec63ee118afdc399cd8
Oscar Abella <oskr.abella@gmail.com>
2022-03-27 22:04:00 -0400

    finished requirements

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,8 @@
 <template>
   <b-container>
-    <b-row class="my-5">
+    <b-row class="my-3">
       <b-col>
-        <h1>Company Inc.</h1>
+        <h1 class="company-title">Company Inc.</h1>
         <NuxtLink to="/">Home</NuxtLink>
         <NuxtLink to="pricing">Pricing</NuxtLink>
       </b-col>
@@ -21,4 +21,16 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+  .nuxt-link-exact-active {
+    font-weight: bold;
+  }
+
+  @media only screen and (max-width: 700px) {
+    h1.company-title {
+      font-size: 1.5rem;
+    }
+  }
+
+
+</style>
