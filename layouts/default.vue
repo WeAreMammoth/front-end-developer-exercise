e2281a7ea66ec011637eb33f4db1f6ef11520102
Kristea Gamel <krgamel@gmail.com>
2022-04-11 15:33:05 -0500

    feature: active page link

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,8 +3,8 @@
     <b-row class="my-5">
       <b-col>
         <h1>Company Inc.</h1>
-        <NuxtLink to="/">Home</NuxtLink>
-        <NuxtLink to="pricing">Pricing</NuxtLink>
+        <NuxtLink to="/" class="nuxt-link-active">Home</NuxtLink>
+        <NuxtLink to="pricing" class="nuxt-link-active">Pricing</NuxtLink>
       </b-col>
     </b-row>
     <b-row>
@@ -21,4 +21,14 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+
+a.nuxt-link-active {
+  font-weight: bold;
+}
+/* exact link will show the primary color for only the exact matching link */
+a.nuxt-link-exact-active {
+  color: #00c58e;
+}
+
+</style>
