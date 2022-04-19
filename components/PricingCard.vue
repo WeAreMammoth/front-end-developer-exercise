e5263ceb11611eeadf164abcd51d6388713df136
Casey Yates <casey@caseyyates.com>
2022-04-19 14:58:28 -0400

    Create PricingCard component

--- components/PricingCard.vue
+++ components/PricingCard.vue
@@ -1,10 +1,49 @@
-<template></template>
+<template>
+  <div
+    class="pricingCard"
+    :class="{
+      'pricingCard--featured': featured,
+    }"
+  >
+    <div class="pricingCard__container">
+      <h3 class="pricingCard__name">{{ name }}</h3>
+      <div class="pricingCard__content">
+        <div class="pricingCard__cost">${{ cost }}/mo</div>
+        <ul class="pricingCard__features-list">
+          <li
+            class="pricingCard_feature"
+            v-for="feature in features"
+            :key="feature"
+          >
+            {{ feature }}
+          </li>
+        </ul>
+        <b-button
+          :variant="paid ? 'primary' : 'outline-primary'"
+          @click="select"
+          >{{ cta }}</b-button
+        >
+      </div>
+    </div>
+  </div>
+</template>
 
 <script>
 export default {
-  name: 'PricingCard',
-  props: {},
+  name: "PricingCard",
+  props: {
+    name: String,
+    cost: Number,
+    features: Array,
+    cta: String,
+    paid: Boolean,
+    featured: Boolean,
+  },
   computed: {},
-  methods: {},
+  methods: {
+    select() {
+      this.$emit("select");
+    },
+  },
 };
 </script>
