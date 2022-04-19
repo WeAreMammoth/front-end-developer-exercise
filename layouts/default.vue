0b0320cb360a06d90b563bc61cd8591cea74b3d0
Casey Yates <casey@caseyyates.com>
2022-04-19 15:10:30 -0400

    Create globally accessible modal

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,23 +1,48 @@
 <template>
-  <b-container>
-    <b-row class="my-5">
-      <b-col>
-        <h1>Company Inc.</h1>
-        <NuxtLink to="/">Home</NuxtLink>
-        <NuxtLink to="pricing">Pricing</NuxtLink>
-      </b-col>
-    </b-row>
-    <b-row>
-      <b-col>
-        <Nuxt />
-      </b-col>
-    </b-row>
-  </b-container>
+  <div>
+    <b-container>
+      <b-row class="my-5">
+        <b-col>
+          <h1>Company Inc.</h1>
+          <NuxtLink to="/">Home</NuxtLink>
+          <NuxtLink to="pricing">Pricing</NuxtLink>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col>
+          <Nuxt />
+        </b-col>
+      </b-row>
+    </b-container>
+    <b-modal
+      :visible="modalShow"
+      title="New Plan Selected"
+      @hide="clearSelectedPlan"
+    >
+      You have chosen: <b>{{ chosenPlan }}</b>
+    </b-modal>
+  </div>
 </template>
 
 <script>
 export default {
   name: "Default",
+  computed: {
+    pricingOptions() {
+      return this.$store.state.pricingOptions;
+    },
+    modalShow() {
+      return !!this.$store.state.chosenPlan;
+    },
+    chosenPlan() {
+      return this.$store.state.chosenPlan;
+    },
+  },
+  methods: {
+    clearSelectedPlan() {
+      this.$store.commit("choosePlan", null);
+    },
+  },
 };
 </script>
 
